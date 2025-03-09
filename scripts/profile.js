039d6ce6280234b02562e4f8544e867b4140d233
katering87 <shishkinak27@gmail.com>
2025-03-09 16:03:02 +0100

    Update profile.js

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let user = JSON.parse(localStorage.getItem("user")) || { email: "", orders: [] };
 
+    document.getElementById("email").value = user.email;
+    document.getElementById("orders").innerText = user.orders.length + " заказов";
+
+    document.getElementById("save-profile").addEventListener("click", function() {
+        user.email = document.getElementById("email").value;
+        localStorage.setItem("user", JSON.stringify(user));
+        alert("Данные сохранены!");
+    });
+});
